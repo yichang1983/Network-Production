8a3fac91e25da3358dcc4e0c38a3c4bbba55be89
yichang1983 <yichang.thomas@gmail.com>
2021-12-01 17:42:12 +0000

    add \n

--- showcmds_arista_cisco.py
+++ showcmds_arista_cisco.py
@@ -7,7 +7,11 @@ device = ConnectHandler(device_type = 'cisco_ios', ip=ipaddress,  username=usern
 output1 = device.send_command("show version")
 output2 = device.send_command("show ip int brief")
 output3 = device.send_command("show int description")
-
+print("show version\n")    #\n 空下一行的意思
 print(output1)
+print("****************************************************************************************************")
+print("show ip int brief\n")
 print(output2)
+print("****************************************************************************************************")
+print ("show int description\n")
 print(output3)
