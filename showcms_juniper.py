95339aa542748be6182a67bd034977f677cb4f38
yichang1983 <yichang.thomas@gmail.com>
2021-12-15 13:22:10 +0000

    Optimization

--- showcms_juniper.py
+++ showcms_juniper.py
@@ -11,25 +11,32 @@ except Exception as E:                  #Exception as E 就是截取全部的錯
 else:
     output1 = device.send_command("show version | match model")
     output2 = device.send_command("show system uptime")
-    output3 = device.send_command("show chassis hardware")
+    output3 = device.send_command("show chassis environment")
     output4 = device.send_command("show chassis routing-engine")
     output5 = device.send_command("show interfaces descriptions")
+
+    print("\n")                    #\n 印出結果後，空一行，感覺才不會跟下一個執行結果擠在一起。
     print("show version | match model:\n")    #\n 空下一行的意思
     print(output1)
+    print("\n")                    #\n 印出結果後，空一行，感覺才不會跟下一個執行結果擠在一起。
     print("****************************************************************************************************")
     print("show system uptime:\n")
     print(output2)
+    print("\n")                    #\n 印出結果後，空一行，感覺才不會跟下一個執行結果擠在一起。
     print("****************************************************************************************************")
-    print("show chassis hardware:\n")
+    print("show chassis environment:\n")
     print(output3)
+    print("\n")                    #\n 印出結果後，空一行，感覺才不會跟下一個執行結果擠在一起。
     print("****************************************************************************************************")
     print("show chassis routing-engine:\n")
     print(output4)
+    print("\n")                    #\n 印出結果後，空一行，感覺才不會跟下一個執行結果擠在一起。
     print("****************************************************************************************************")
-    print("show ip int brief:\n")
+    print("show interfaces descriptions:\n")
     print(output5)
+    print("\n")                    #\n 印出結果後，空一行，感覺才不會跟下一個執行結果擠在一起。
 finally:
-    print("close")
+    print("Close")
 
 
 
